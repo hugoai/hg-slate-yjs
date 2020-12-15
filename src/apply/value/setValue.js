dec08a2105e633d993a06730b2cce3d5be4e769d
Alfredo Neto <jarn40@gmail.com>
2020-12-15 12:57:30 -0300

    Added a check on setValue function to garantee the data field exist

--- src/apply/value/setValue.js
+++ src/apply/value/setValue.js
@@ -1,9 +1,12 @@
-
+const Y = require('yjs');
 
 const setValue = (doc, op)=>{
+    if(!doc.hasOwnProperty('data')){
+        doc.set("data", new Y.Map())
+    }
     const syncData = doc.get('data')
 
-    for (const [key, value] of Object.entries(op.properties)) {
+    for (const [key, value] of Object.entries(op.properties.data.toJSON())) {
         syncData.set(key, value)
     }
     
