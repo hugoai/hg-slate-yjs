3388c36b785fca4c56a43b51f055d82e0b84cb2d
Alfredo Neto <jarn40@gmail.com>
2020-12-15 19:00:01 -0300

    Review: Removing unnecessary check.

--- src/apply/value/setValue.js
+++ src/apply/value/setValue.js
@@ -1,9 +1,8 @@
 const Y = require('yjs');
 
 const setValue = (doc, op)=>{
-    if(!doc.hasOwnProperty('data')){
-        doc.set("data", new Y.Map())
-    }
+    doc.set("data", new Y.Map())
+    
     const syncData = doc.get('data')
 
     for (const [key, value] of Object.entries(op.properties.data.toJSON())) {
