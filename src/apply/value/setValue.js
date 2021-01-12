a16a56a96f7d812f33351c46cb3161806f2fb394
Kirk Johnson <kjohnson@hugo.team>
2021-01-12 15:00:54 -0700

    Change setValue.js to handle missing data property gracefully
    * Eliminates 'TypeError: Cannot read property 'toJSON' of undefined' errors that were spamming console

--- src/apply/value/setValue.js
+++ src/apply/value/setValue.js
@@ -1,15 +1,15 @@
 const Y = require('yjs');
 
-const setValue = (doc, op)=>{
+const setValue = (doc, op) => {
+  if (op.properties && op.properties.data) {
     const newData = new Y.Map()
-
     for (const [key, value] of Object.entries(op.properties.data.toJSON())) {
-        newData.set(key, value)
+      newData.set(key, value);
     }
+    doc.set("data", newData);
+  }
 
-    doc.set("data", newData)
-    
-    return doc
+  return doc;
 }
 
 
