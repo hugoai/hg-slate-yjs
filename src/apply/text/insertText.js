c849b615890fe5d8e201ff9fda4b79f3fe287d53
Kirk Johnson <70281324+kjohnson-hugo@users.noreply.github.com>
2021-01-12 11:14:25 -0700

    Add mark handling for insert_text ops in Slate->Yjs path (#22)

--- src/apply/text/insertText.js
+++ src/apply/text/insertText.js
@@ -1,5 +1,6 @@
 const { SyncElement } = require('../../model');
 const { getTarget } = require('../../path');
+const { toFormattingAttributes } = require('../../utils');
 
 /**
  * Applies a insert text operation to a SyncDoc.
@@ -9,8 +10,9 @@ const { getTarget } = require('../../path');
 const insertText = (doc, op) => {
   const syncDoc = doc.get('document')
   const node = getTarget(syncDoc, op.path);
+  const format = toFormattingAttributes(op.marks);
   const nodeText = SyncElement.getText(node);
-  nodeText.insert(op.offset, op.text);
+  nodeText.insert(op.offset, op.text, format);
   return doc;
 };
 
