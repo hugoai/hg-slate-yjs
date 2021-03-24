f404494d7bed38c2b78e029fc334561131341fe3
Alfredo <jarn.dev@gmail.com>
2021-03-23 23:22:43 -0300

    chore: add more types

--- src/types/types.d.ts
+++ src/types/types.d.ts
@@ -1,8 +1,12 @@
 import * as Y from 'yjs';
-import { Block, Inline, Text } from 'slate';
+import { Iterable } from 'immutable';
+import { Block, Inline, Text, Operation } from 'slate';
 
 export type SyncDoc = Y.Map<any>;
 export type SyncArray = Y.Array<SyncDoc>;
-export type SyncNodeType = SyncDoc | SyncDoc;
+export type SyncNodeType = SyncDoc | SyncArray;
 
 export type Node = Block | Inline | Text;
+
+export type SlateOperation = Operation;
+export type SlatePath = Iterable<number>;
